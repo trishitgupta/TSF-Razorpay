d6cc48fc7b0f5edeed5b6094e9c8517ffc356417
Trishit Gupta <trishit.gupta2610@gmail.com>
2022-07-12 03:01:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import D from './components/mainpage';
+
+function App() {
+  return (
+    <div className="App">
+      <D/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mainpage.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+
+
+const disHome = () => {
+ 
+
+  
+  return (
+    <>
+   
+    <section>
+  <header>
+    <h2>
+      <a href="#" className="logo">
+        TSF
+      </a>
+    </h2>
+    <div className="navigation">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Info</a>
+      <a href="#">Services</a>
+      <a href="#">Contact</a>
+    </div>
+  </header>
+  <div className="content">
+    <div className="info">
+      <h2>
+        Doing Nothing is not <br />
+        <span>an option of our life !</span>
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+      <a href="https://rzp.io/l/gD20V0Gfe" target="_self" className="info-btn">
+        Donate Now
+      </a>
+    </div>
+  </div>
+  <div className="media-icons">
+    <a href="#">
+      <i className="fab fa-facebook-f" />
+    </a>
+    <a href="#">
+      <i className="fab fa-twitter" />
+    </a>
+    <a href="https://www.instagram.com/trishit.gupta2610/" target="_blank">
+      <i className="fab fa-instagram" />
+    </a>
+    
+   
+    
+
+    
+  </div>
+  <div> 
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase font-weight-bold">Major Causes</h2>
+                <h3 class="section-subheading text-muted">
+                  Lorem ipsum dolor sit amet consectetur.</h3>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                      <i class="fas fa-circle fa-stack-2x text-danger"></i>
+                      <i class="fas fa-hand-holding-usd fa-stack-1x fa-inverse"></i>
+                      
+                    </span>
+                    <h4 class="my-3">Give Donations</h4>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur 
+                      adipisicing elit. Minima maxime quam architecto quo inventore 
+                      harum ex magni, dicta impedit.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                      <i class="fas fa-circle fa-stack-2x text-warning"></i>
+                      <i class="fas fa-lightbulb fa-stack-1x fa-inverse"></i>
+                      </span>
+                    <h4 class="my-3">Give Inspiration</h4>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, 
+                      consectetur adipisicing elit. Minima maxime quam 
+                      architecto quo inventore harum ex magni, dicta impedit.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                      <i class="fas fa-circle fa-stack-2x text-success"></i>
+                      <i class="fas fa-tshirt fa-stack-1x fa-inverse"></i>
+                    </span>
+                   
+                    <h4 class="my-3">Become Volunteer</h4>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur 
+                      adipisicing elit. Minima maxime quam architecto quo inventore
+                       harum ex magni, dicta impedit.</p>
+                </div>
+            </div>
+        </div>
+   
+      
+    </div>
+</section>
+
+
+    
+    
+    </>
+  );
+};
+
+
+export default disHome;
